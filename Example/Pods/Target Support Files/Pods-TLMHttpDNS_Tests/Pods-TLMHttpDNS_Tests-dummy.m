cfd6346c230a316f02399ef055847eaaae5d7d55
Tong Leiming <tongleiming@daling.com>
2019-05-16 20:18:41 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-TLMHttpDNS_Tests/Pods-TLMHttpDNS_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TLMHttpDNS_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TLMHttpDNS_Tests
+@end
---
+++ Example/Pods/Target Support Files/TLMHttpDNS/TLMHttpDNS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TLMHttpDNS : NSObject
+@end
+@implementation PodsDummy_TLMHttpDNS
+@end

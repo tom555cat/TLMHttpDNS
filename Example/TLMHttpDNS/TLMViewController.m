97c4e2bb1fa45070ba99907dd63501eeec3b7928
Tong Leiming <tongleiming@daling.com>
2019-05-19 12:16:51 +0800

    添加测试页面

--- Example/TLMHttpDNS/TLMViewController.m
+++ Example/TLMHttpDNS/TLMViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "TLMViewController.h"
+#import "TestViewController.h"
+#import "PostTestViewController.h"
+#import "WKWebViewTestController.h"
 
-@interface TLMViewController ()
+@interface TLMViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray *dataArray;
 
 @end
 
@@ -18,12 +24,49 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    self.dataArray = @[@"正常测试", @"Post请求测试", @"WKWebView测试"];
+    
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
+    [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableViewDelegate & UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dataArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"HttpDNSCell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"HttpDNSCell"];
+    }
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        TestViewController *vc = [[TestViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+    } else if (indexPath.row == 1) {
+        PostTestViewController *vc = [[PostTestViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+    } else if (indexPath.row == 2) {
+        WKWebViewTestController *vc = [[WKWebViewTestController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+    }
+    return;
 }
 
 @end
---
+++ Example/TLMHttpDNS/TestViewController.m
@@ -0,0 +1,33 @@
+//
+//  TestViewController.m
+//  DNSTest
+//
+//  Created by tongleiming on 2019/5/6.
+//  Copyright © 2019 tongleiming. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    NSString *url = [NSString stringWithFormat:@"https://www.baidu.com"];
+    NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+    }];
+    [dataTask resume];
+}
+
+@end
